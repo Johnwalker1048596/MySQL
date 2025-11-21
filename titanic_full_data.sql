a7b65f1a02cd2846ae92d2352b9a9f918f3b11c3
Johnwalker1048596 <li5999613@gmail.com>
2025-11-22 02:25:17 +0800

    <11/22>

--- titanic_full_data.sql
+++ titanic_full_data.sql
@@ -34,3 +34,63 @@ IGNORE 1 ROWS
 SET age = NULLIF(@age,''), fare = nullif(@fare, ''), body = nullif(@body, '');
 
 select * from full_passengers limit 1350;
+
+#1
+select sex, count(sex) gender_counts
+from full_passengers
+group by sex;
+
+#2
+select id, pname
+from full_passengers
+where id between 591 and 883;
+
+#3
+select pname, survived
+from full_passengers 
+where pname like 'Andersson%' and homedest = 'Sweden Winnipeg, MN';
+
+#4
+select *
+from full_passengers
+where ticket = 347080;
+
+#5
+select id, pclass, pname
+from full_passengers
+where sex = 'male'
+  and pname like '% Leonard%';
+  
+#6
+select ticket,count(ticket) ticket_count
+from full_passengers
+group by ticket
+order by ticket_count desc
+limit 1;
+
+#7
+-- select pclass, round(avg(age), 2) avg_age
+select pclass, (avg(age)) avg_age
+from full_passengers
+where sex = 'male' and pclass in (2, 3)
+group by pclass
+order by pclass;
+
+#8
+select embarked,
+       count(*) embark_count,
+       round(count(*) * 100 / 1309, 2) embark_pct
+from full_passengers
+where embarked in ('S','C','Q')
+group by embarked
+order by embark_count desc;
+
+-- select embarked,
+--        count(*) embark_count,
+--        round(count(*) * 100 / (select count(*) from full_passengers), 2) embark_pct
+-- from full_passengers
+-- where embarked in ('S','C','Q')
+-- group by embarked
+-- order by embark_count desc;
+
+
